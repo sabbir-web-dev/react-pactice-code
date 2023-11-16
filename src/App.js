810a916f2e2fe4734128a2e2da442899858b4893
Sabbir Hosen <125633036+tssabbir@users.noreply.github.com>
2023-11-16 13:28:44 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 import AlinOne from "./Components/AlInOne/AlinOne"; 
-import MamoUseCallbacke from './Components/UseMamo&useCallbacke/MamoUseCallbacke'
 
 
 
@@ -12,7 +11,6 @@ function App() {
 
   return (
     <div className="wrap">
-      {/* <MamoUseCallbacke /> */}
     <AlinOne /> 
     </div>
   );
